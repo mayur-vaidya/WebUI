641e614bb7fbdb0eebee6ffd172c5e6fc9a887af
mayurvaidya09 <mayur@newput.com>
2016-11-03 13:35:30 +0530

    Book module with states and custom directives.

--- AngularLibrary/JS/bookModule.js
+++ AngularLibrary/JS/bookModule.js
@@ -11,6 +11,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
           url: '/:bookName/page:page',
           templateUrl: 'tiles.html'
       })
+      .state('homepage.book.image',  {
+        url: '/:bookId'
+      })
       .state('homepage.contact', {
         url: '/contactUs',
         templateUrl: 'contact.html'
@@ -18,14 +21,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .state('homepage.view', {
       url: '/view',
       templateUrl: 'showAll.html'
-  })
-  .state('homepage.book.image', {
-      url: '/:bookId',
-      templateUrl: 'image-modal.html'
-  })
+    })
 });
 
-app.directive('numbersOnly', function () {
+app.directive('numbersOnly', function() {
   return {
     require: 'ngModel',
     link: function(scope, element, attr, ngModelCtrl) {
@@ -61,4 +60,36 @@ app.directive('imageNotFound', function() {
      });
     }
   }
+});
+
+app.directive('modal', function () {
+  return {
+    template: '<div class="modal fade" data-backdrop="static">' +
+              '<div class="modal-body" ng-transclude> </div>' +
+              '</div>',
+    restrict: 'E',
+    transclude: true,
+    replace:true,
+    scope:true,
+    link: function postLink(scope, element, attrs) {
+      scope.$watch(attrs.visible, function(value){
+        if(value == true)
+          $(element).modal('show');
+        else
+          $(element).modal('hide');
+      });
+
+      $(element).on('shown.bs.modal', function(){
+        scope.$apply(function(){
+          scope.$parent[attrs.visible] = true;
+        });
+      });
+
+      $(element).on('hidden.bs.modal', function(){
+        scope.$apply(function(){
+          scope.$parent[attrs.visible] = false;
+        });
+      });
+    }
+  };
 });
